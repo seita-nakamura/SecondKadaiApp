c00a9ba5d0e026329043589adca11e505285b97e
seita-nakamura <poke.basket@icloud.com>
2021-02-21 17:33:59 +0900

    SecondKadaiApp

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textbox: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
-
-
-}
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name=textbox.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){}}
 
